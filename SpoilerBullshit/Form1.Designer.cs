59a9e9789ee7f30c084cf957fd44e5b36def810a
Dark Kitsu <cbmuench@hotmail.com>
2019-05-14 19:44:52 -0700

    First commit

---
+++ SpoilerBullshit/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace SpoilerBullshit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.OutputTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.InputTextBox.Location = new System.Drawing.Point(13, 37);
+            this.InputTextBox.Multiline = true;
+            this.InputTextBox.Name = "textBox1";
+            this.InputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.InputTextBox.Size = new System.Drawing.Size(775, 80);
+            this.InputTextBox.TabIndex = 0;
+            this.InputTextBox.TextChanged += new System.EventHandler(this.InputTextBox_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.OutputTextBox.Location = new System.Drawing.Point(12, 149);
+            this.OutputTextBox.Multiline = true;
+            this.OutputTextBox.Name = "textBox2";
+            this.OutputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.OutputTextBox.Size = new System.Drawing.Size(775, 400);
+            this.OutputTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Input";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Output";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(800, 561);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OutputTextBox);
+            this.Controls.Add(this.InputTextBox);
+            this.Name = "Form1";
+            this.Text = "SpoilerBullshit";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.TextBox OutputTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ SpoilerBullshit/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SpoilerBullshit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void InputTextBox_TextChanged(object sender, EventArgs e)
+        {
+            var joined = string.Concat(InputTextBox.Text.Select(c => $"||{c}||"));
+            var lines = new string[(joined.Length / 990 + 1) * 2];
+            var len = joined.Length / 990 + 1;
+            for (var i = 0; i < len; i++)
+            {
+                lines[i * 2 + 1] = "";
+                lines[i * 2] = joined.Substring(0, Math.Min(990, joined.Length));
+                joined = joined.Substring(Math.Min(990, joined.Length));
+            }
+            OutputTextBox.Lines = lines;
+        }
+    }
+}
